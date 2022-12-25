2f8ff0aa56eea377c880189e293a1374697ade18
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2022-12-25 20:50:47 +0530

    T.C-O(N^2)

---
+++ 1. Two Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int a[]=new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=1;j<nums.length;j++){
+                if(nums[i]==nums[j] && nums[i]+nums[j]==target && i<j && i==0){
+                    a[1]=j;
+                    break;
+                }
+                if(nums[i]+nums[j]==target && i<j){
+                    a[0]=i;
+                    a[1]=j;
+                    break;
+                }
+            }
+        }
+        return a;
+        
+    }
+}
