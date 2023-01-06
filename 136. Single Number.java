1819a9e2c480b6b0b9082b3fe25d2f6865334978
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-01-06 19:28:43 +0530

    Create 136. Single Number.java

---
+++ 136. Single Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer>map=new LinkedHashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer>entry: map.entrySet()){
+           if(entry.getValue()==1){
+               return entry.getKey();
+           }
+       }
+        return -1;
+
+    }
+}
