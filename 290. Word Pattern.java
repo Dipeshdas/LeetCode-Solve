5bf031f85d52e7fe53b7a49b8ede96b58a0bd293
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-01-12 12:05:44 +0530

    Update 290. Word Pattern.java

--- 290. Word Pattern.java
+++ 290. Word Pattern.java
@@ -2,17 +2,27 @@ class Solution {
     public boolean wordPattern(String pattern, String s) {
         
         String arr[]=s.split(" ");
-        Map h=new HashMap<>();
+        Map<Character,String> map=new HashMap<>();
+        Set<String>h=new HashSet<>();
         
         if(arr.length!=pattern.length()){
             return false;
         }
         
-        for(Integer i=0;i<pattern.length();i++){
-            if(h.put(arr[i],i)!=h.put(pattern.charAt(i),i)){
+        boolean flag=false;
+        for(int i=0;i<pattern.length();i++){
+            if(!map.containsKey(pattern.charAt(i)) && !h.contains(arr[i])){
+                map.put(pattern.charAt(i),arr[i]);
+                h.add(arr[i]);
+            }
+            if(map.containsKey(pattern.charAt(i)) && map.get(pattern.charAt(i)).equals(arr[i])){
+                flag=true;
+            }
+            else{
                 return false;
             }
+
         }
-        return true;
+        return flag;
     }
 }
