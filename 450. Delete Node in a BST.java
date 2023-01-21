690e8458ed7cfe5d5338e368fc1a628ed3eadf38
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-01-21 12:37:16 +0530

    Create 450. Delete Node in a BST.java

---
+++ 450. Delete Node in a BST.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return root;
+
+        if(key<root.val){
+            root.left=deleteNode(root.left,key);
+        }
+        else if(key>root.val){
+            root.right=deleteNode(root.right,key);
+        }
+        else{
+            if(root.left==null) return root.right;
+            if(root.right==null) return root.left;
+
+            root.val=minvalue(root.right);//to find inorder succesor
+
+            root.right=deleteNode(root.right,root.val);
+        }
+        return root;
+    }
+
+    public int minvalue(TreeNode root){
+        int min=root.val;
+        while(root.left!=null){
+            min=root.left.val;
+            root=root.left;
+        }
+        return min;
+    }
+}
