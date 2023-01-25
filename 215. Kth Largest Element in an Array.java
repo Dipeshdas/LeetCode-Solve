6f62962850031988d6122fa61c666860dfaebff0
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-01-25 22:46:18 +0530

    Create 215. Kth Largest Element in an Array.java

---
+++ 215. Kth Largest Element in an Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            pq.add(nums[i]);
+        }
+        for(int i=k;i<nums.length;i++){
+            if(pq.peek()<nums[i]){
+                pq.poll();  
+                pq.add(nums[i]);
+            }
+        }
+        return pq.peek();
+    }
+}
