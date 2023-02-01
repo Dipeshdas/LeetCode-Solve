f070d37460a3f22338bf042db50700eb1435e688
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-02-01 15:55:39 +0530

    Create 802. Find Eventual Safe States.java

---
+++ 802. Find Eventual Safe States.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n=graph.length;
+        int visit[]=new int[n];
+        int pathVisit[]=new int[n];
+        int check[]=new int[n];
+        
+
+        for(int i=0;i<n;i++){
+            if(visit[i]==0){
+                dfs(i,visit,pathVisit,check,graph);
+            }
+        }
+
+        List<Integer>ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(check[i]==1) ans.add(i);
+        }
+        return ans;
+
+    }
+
+    public boolean dfs(int i,int visit[],int pathVisit[],int check[],int[][] graph){
+        visit[i]=1;
+        pathVisit[i]=1;
+        check[i]=0;
+
+        for(int e:graph[i]){
+            if(visit[e]==0){
+                if(dfs(e,visit,pathVisit,check,graph)){
+                    return true;
+                }
+            }
+            else if(pathVisit[e]==1) return true;
+        }
+        check[i]=1;
+        pathVisit[i]=0;
+        return false;
+    }
+}
