9d0a2386bec6d5beb3a27dfaf75ca7e5af9efdee
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-04-01 12:46:00 +0530

    Create 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.java

---
+++ 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    TreeNode ans=null;
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        
+        return preOrder(original,cloned,target);   
+    }
+
+    public final TreeNode preOrder(final TreeNode original, final TreeNode cloned, final TreeNode target){
+        if(original==null){
+            return null;
+        }
+        if(target==original){
+            ans=cloned;
+        }
+        preOrder(original.left,cloned.left,target);
+        preOrder(original.right,cloned.right,target);
+        return ans;
+    }
+}
