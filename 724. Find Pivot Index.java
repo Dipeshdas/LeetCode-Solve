3912e11f889fa772104f6ab1430cac663a75d26b
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-04-29 11:27:56 +0530

    Create 724. Find Pivot Index.java

---
+++ 724. Find Pivot Index.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int rightSum=0;
+        int leftSum=0;
+
+        for(int i=0;i<nums.length;i++){
+            rightSum+=nums[i];
+        }
+
+        for(int i=0;i<nums.length;i++){
+            rightSum-=nums[i];
+            if(rightSum==leftSum){
+                return i;
+            }
+            leftSum+=nums[i];
+            
+        }
+        return -1;
+
+    }
+}
