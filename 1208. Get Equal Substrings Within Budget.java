1d77771f2bf36c5961670b6276b8c92f00eef2e4
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-05-07 12:48:40 +0530

    Create 1208. Get Equal Substrings Within Budget.java

---
+++ 1208. Get Equal Substrings Within Budget.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int i=0;
+        int j=0;
+        int ans=0;
+        int max=0;
+
+        while(j<s.length()){
+            ans+=Math.abs((int)s.charAt(j)-(int)t.charAt(j));
+            if(ans<=maxCost){
+                max=Math.max(max,j-i+1);
+            }
+             
+            else {
+                while(ans>maxCost){
+                    ans-=Math.abs((int)s.charAt(i)-(int)t.charAt(i));
+                    i++;
+                }
+            }
+            j++;
+
+        }
+        return max;
+    }
+}
