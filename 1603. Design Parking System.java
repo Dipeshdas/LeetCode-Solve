96a1606f5a18d34008c2156b845a1313a697d91c
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-05-29 09:18:07 +0530

    Create 1603. Design Parking System.java

---
+++ 1603. Design Parking System.java
@@ -0,0 +1,38 @@
+class ParkingSystem {
+    int slot[]=new int[3];
+    public ParkingSystem(int big, int medium, int small) {
+        slot[0]=big;
+        slot[1]=medium;
+        slot[2]=small;
+    }
+    
+    public boolean addCar(int carType) {
+        if(carType==1){
+            if(slot[0]==0){
+                return false;
+            }
+            slot[0]--;
+            return true;
+        }
+        else if(carType==2){
+            if(slot[1]==0){
+                return false;
+            }
+            slot[1]--;
+            return true;
+        }
+        else{
+            if(slot[2]==0){
+                return false;
+            }
+            slot[2]--;
+            return true;
+        }
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
