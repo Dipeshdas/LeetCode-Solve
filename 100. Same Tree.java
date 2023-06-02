afe9adbaf3d507e49b8a0704e62a288cd66a6c89
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2023-06-02 14:19:23 +0530

    Update 100. Same Tree.java

--- 100. Same Tree.java
+++ 100. Same Tree.java
@@ -24,8 +24,7 @@ class Solution {
         
         if(p.val!=q.val){
             return false;
-        }
-        
+        }       
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
